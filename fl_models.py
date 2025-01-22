8f37e8e2910c4ffe371f08e139943f38c7d86de1
Ashley0909 <ashleyau09@gmail.com>
2025-01-22 18:51:14 +0000

    Added function to customise PE shape, and only import PE during training

--- fl_models.py
+++ fl_models.py
@@ -203,12 +203,12 @@ class ReshapeH():
     def __init__(self, empty_h):
         self.empty_h = empty_h
 
-    def reshape_to_fill(self, h, subnodes, layer):
-        if h.shape == self.empty_h[layer].shape: # only NC will have misaligned NE
+    def reshape_to_fill(self, h, subnodes):
+        if h.shape == self.empty_h.shape: # only NC will have misaligned NE
             return h
         
         # reshaped = self.empty_h[layer]
-        reshaped = copy.deepcopy(self.empty_h[layer])
+        reshaped = copy.deepcopy(self.empty_h)
         reshaped[subnodes] = h
 
         return reshaped
@@ -225,13 +225,11 @@ class ROLANDGNN(torch.nn.Module):
         self.device = device
         hidden_conv_1 = 64
         hidden_conv_2 = 32
-        empty_h = [torch.zeros((num_nodes, hidden_conv_1)).to(self.device), torch.zeros((num_nodes, hidden_conv_2)).to(self.device)]
         self.preprocess1 = Linear(input_dim, 256).to(self.device)
         self.preprocess2 = Linear(256, 128).to(self.device)
         self.conv1 = GCNConv(128, hidden_conv_1, add_self_loops=False).to(self.device)
         self.conv2 = GCNConv(hidden_conv_1, hidden_conv_2, add_self_loops=False).to(self.device)
         self.postprocess1 = Linear(hidden_conv_2, output_dim).to(self.device)
-        self.reshape = ReshapeH(empty_h)
         
         #Initialize the loss function to BCEWithLogitsLoss
         self.loss_fn = nn.BCEWithLogitsLoss()
@@ -251,8 +249,7 @@ class ROLANDGNN(torch.nn.Module):
         else:
             assert(update>=0 and update <=1)
             self.tau = torch.Tensor([update]).to(self.device)
-        self.previous_embeddings = [torch.zeros((num_nodes, hidden_conv_1)).to(self.device),
-                                    torch.zeros((num_nodes, hidden_conv_2)).to(self.device)]
+        self.previous_embeddings = [torch.zeros((num_nodes, hidden_conv_1)).to(self.device), torch.zeros((num_nodes, hidden_conv_2)).to(self.device)]
         
     def reset_parameters(self):
         self.preprocess1.reset_parameters()
@@ -261,12 +258,21 @@ class ROLANDGNN(torch.nn.Module):
         self.conv2.reset_parameters()
         self.postprocess1.reset_parameters()
 
+    def customise_pe(self, num_nodes):
+        empty_h = torch.zeros((num_nodes, 64)).to(self.device) # To reshape after the 1st conv layer
+        self.reshape = ReshapeH(empty_h)
+
+        self.previous_embeddings = [torch.zeros((num_nodes, 64)).to(self.device), torch.zeros((num_nodes, 32)).to(self.device)]
+
+
     def forward(self, x, edge_index, task_type, edge_label_index=None, subnodes=None, previous_embeddings=None, num_current_edges=None, num_previous_edges=None):        
         #You do not need all the parameters to be different to None in test phase
         #You can just use the saved previous embeddings and tau
         if previous_embeddings is not None: #None if test
             self.previous_embeddings = [previous_embeddings[0].clone().to(self.device),previous_embeddings[1].clone().to(self.device)]
             train = True
+        else:
+            train = False
         if self.update=='moving' and num_current_edges is not None and num_previous_edges is not None: #None if test
             #compute moving average parameter
             self.tau = torch.Tensor([num_previous_edges / (num_previous_edges + num_current_edges)]).clone().to(self.device) # tau -- past weight
@@ -290,9 +296,7 @@ class ROLANDGNN(torch.nn.Module):
         h = F.dropout(h, p=self.dropout,inplace=True)
 
         #Reshape h for NC (clients have different number of nodes)
-        h = self.reshape.reshape_to_fill(h, subnodes, 0)
-
-        print("Done Reshape")
+        h = self.reshape.reshape_to_fill(h, subnodes)
 
         #Embedding Update after first layer (only when training)
         if train == True:
